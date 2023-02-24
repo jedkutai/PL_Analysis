99c30fa30271a18ab9b26e4784a6727176d380ef
Jed <jedkutai@gmail.com>
2023-02-24 16:59:41 -0500

    updated league class

--- TeamClass/TeamFunctions.py
+++ TeamClass/TeamFunctions.py
@@ -15,16 +15,3 @@ def match_information(match_file, match_dict):
         name = d[2]+d[4]+d[8]
         match_dict[name] = entry
 
-def update_clubs(match_file, league_object):
-    mf = open(match_file, "r")
-    mf.readline() #skip header
-    for line in mf:
-        d = line.split(",") # d is the data chopped up into a list, im just too lazy to write that a bunch of times
-        m = d[2].split("-")
-        if d[6] == "":
-            continue
-        
-        s = d[6].split("–") # Score THE - is NOT A REGULAR  -
-        league_object.get_club(d[4]).add_home_match(d[8],s[0],s[1])
-        league_object.get_club(d[8]).add_away_match(d[4],s[1],s[0])
-        
